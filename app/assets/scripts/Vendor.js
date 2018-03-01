4c01cc3ed02320230fe39b75addbd926f30258d1
Violeta <violetr.23@gmail.com>
2018-03-01 16:16:32 -0500

    Added browser support fallbacks.

--- app/assets/scripts/Vendor.js
+++ app/assets/scripts/Vendor.js
@@ -1,3 +1,7 @@
+import "../../temp/scripts/modernizr";
+/* Para que las images responsive funcionen en todos los
+navegadores*/
+import "picturefill";
 /*Cuando utilizamos packages como lazy sozes que se ejecutan
 por su cuenta, sin necesidad de que escribamos codigo JS,
 es una buena practica importalor en un archivo independiente*/
